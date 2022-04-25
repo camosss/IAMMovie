97cd8d9a05b9878b5844401bfc2addfdaa05392f
HoSeong <camosss@naver.com>
2022-04-25 18:44:11 +0900

    feature: SearchBar, TableView 생성

--- MovieProject/Presentation/SearchMovie/SearchMovieViewController.swift
+++ MovieProject/Presentation/SearchMovie/SearchMovieViewController.swift
@@ -6,8 +6,14 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SearchMovieViewController: BaseViewController {
+
+    // MARK: - Properties
+
+    private let searchBar = UISearchBar()
+    private let tableView = UITableView()
 
     // MARK: - Lifecycle
 
@@ -20,14 +26,27 @@ final class SearchMovieViewController: BaseViewController {
 
     override func setViews() {
         super.setViews()
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
     }
 
     override func setConstraints() {
         super.setConstraints()
+        searchBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(50)
+        }
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
     }
 
     override func setConfigurations() {
         super.setConfigurations()
         configureLeftBarButtonItem(title: "네이버 영화 검색")
+
+        searchBar.backgroundImage = UIImage()
+        searchBar.placeholder = "영화를 검색해주세요..."
     }
 }
