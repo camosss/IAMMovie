f3ed40e972d1f50a01c0e1102d9d7afa7653e1de
HoSeong <camosss@naver.com>
2022-04-25 17:54:12 +0900

    Initial Commit

---
+++ MovieProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieProject
+//
+//  Created by 강호성 on 2022/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
