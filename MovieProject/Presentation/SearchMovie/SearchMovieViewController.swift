e06b6a3e2cbcc9309b0a76ddcf6282520778477f
HoSeong <camosss@naver.com>
2022-04-29 13:41:08 +0900

    edit: 디테일뷰로 이동 시, moveList 초기화

--- MovieProject/Presentation/SearchMovie/SearchMovieViewController.swift
+++ MovieProject/Presentation/SearchMovie/SearchMovieViewController.swift
@@ -170,6 +170,7 @@ final class SearchMovieViewController: BaseViewController {
                 let movie = self.viewModel.movieList.value[indexPath.row]
                 let controller = DetailMovieViewController(movie: movie)
                 self.navigationController?.pushViewController(controller, animated: true)
+                self.viewModel.movieList.accept([]) /// movieList 초기화
             })
             .disposed(by: disposeBag)
 
