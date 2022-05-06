fce38c18f76f722b900dcb3475f47a2773a0e10b
HoSeong <camosss@naver.com>
2022-05-06 13:32:26 +0900

    delete: ImageCache (Kingfisher 내부에 메서드 존재)

--- MovieProject/Extension/UIImageView+Ex.swift
+++ MovieProject/Extension/UIImageView+Ex.swift
@@ -10,19 +10,6 @@ import Kingfisher
 
 extension UIImageView {
     func setImage(with urlString: String) {
-        ImageCache.default.retrieveImage(forKey: urlString, options: nil) { result in
-            switch result {
-            case .success(let value):
-                if let image = value.image { /// 캐시가 존재하는 경우
-                    self.image = image
-                } else {                     /// 캐시가 존재하지 않는 경우
-                    guard let url = URL(string: urlString) else { return }
-                    let resource = ImageResource(downloadURL: url, cacheKey: urlString)
-                    self.kf.setImage(with: resource)
-                }
-            case .failure(let error):
-                print(error)
-            }
-        }
+        self.kf.setImage(with: URL(string: urlString))
     }
 }
