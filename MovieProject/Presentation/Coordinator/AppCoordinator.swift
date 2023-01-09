b4897dca1d0feefc724fd0eeb3c3dd7ef67f0842
camosss <camosss@naver.com>
2023-01-09 18:53:22 +0900

    feat: Coordinator 생성

---
+++ MovieProject/Presentation/Coordinator/AppCoordinator.swift
@@ -0,0 +1,48 @@
+//
+//  AppCoordinator.swift
+//  MovieProject
+//
+//  Created by 강호성 on 2023/01/09.
+//
+
+import UIKit
+
+/// 시작 흐름 정의 프로토콜
+protocol AppCoordinatorProtocol: Coordinator {
+    func connectMainFlow()
+}
+
+/// 모든 흐름의 시작점
+final class AppCoordinator: AppCoordinatorProtocol {
+
+    weak var delegate: CoordinatorDelegate?
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    var type: CoordinatorStyleCase = .main
+
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        navigationController.setNavigationBarHidden(false, animated: false)
+    }
+
+    func start() {
+        connectMainFlow()
+    }
+
+    func connectMainFlow() {
+
+    }
+}
+
+// MARK: - CoordinatorDelegate
+extension AppCoordinator: CoordinatorDelegate {
+    func didFinish(childCoordinator: Coordinator) {
+        childCoordinators = childCoordinators.filter { $0.type != childCoordinator.type }
+
+        navigationController.viewControllers.removeAll()
+        switch childCoordinator.type {
+        case .main:
+            connectMainFlow()
+        }
+    }
+}
---
+++ MovieProject/Presentation/Coordinator/Coordinator.swift
@@ -0,0 +1,36 @@
+//
+//  Coordinator.swift
+//  MovieProject
+//
+//  Created by 강호성 on 2023/01/09.
+//
+
+import UIKit
+
+protocol CoordinatorDelegate: AnyObject {
+    func didFinish(childCoordinator: Coordinator)
+}
+
+enum CoordinatorStyleCase {
+    case main
+}
+
+protocol Coordinator: AnyObject {
+
+    var delegate: CoordinatorDelegate? { get set }
+    var navigationController: UINavigationController { get set }
+    var childCoordinators: [Coordinator] { get set }
+    var type: CoordinatorStyleCase { get }
+
+    func start()
+    func finish()
+
+    init(_ navigationController: UINavigationController)
+}
+
+extension Coordinator {
+    func finish() {
+        childCoordinators.removeAll()
+        delegate?.didFinish(childCoordinator: self)
+    }
+}
